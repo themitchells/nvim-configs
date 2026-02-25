468e3100646c57bddee98c64396705a83767e8f6
Jerias Mitchell <jerias.mitchell@viavisolutions.com>
2026-02-24 19:49:08 -0500

    Update plugins, fix nvim-surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -5,19 +5,4 @@ return {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
-    opts = {
-        -- Use default keymaps (ys, ds, cs)
-        keymaps = {
-            insert = "<C-g>s",
-            insert_line = "<C-g>S",
-            normal = "ys",
-            normal_cur = "yss",
-            normal_line = "yS",
-            normal_cur_line = "ySS",
-            visual = "S",
-            visual_line = "gS",
-            delete = "ds",
-            change = "cs",
-        },
-    },
 }

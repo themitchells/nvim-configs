2ab37ae83d1efdd15f8a1fab59d28ba8f6a77379
Jerias Mitchell <jerias.mitchell@viavisolutions.com>
2026-03-06 14:34:42 -0500

    Tweak to buffergator config

--- lua/plugins/buffergator.lua
+++ lua/plugins/buffergator.lua
@@ -1,5 +1,4 @@
 -- nvim-buffergator: Lua rewrite of vim-buffergator
--- Local plugin at ~/mywcps/other/nvim-buffergator
 
 return {
     "themitchells/nvim-buffergator",
@@ -7,10 +6,10 @@ return {
         require("nvim-buffergator").setup({
             width      = 30,
             min_width  = 20,
-            max_width  = 60,
+            max_width  = 120,
             sort       = "filepath",
             auto_resize = true,
-            path       = 1,
+            path       = 4,
         })
     end,
 }
